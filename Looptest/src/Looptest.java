6501c60dc9022eff9a1a76523de054f365342dac
CONNORB57157 <cbensley.57157@farnborough.ac.uk>
2015-11-03 11:45:16 +0000

    03/11/15

---
+++ Looptest/src/Looptest.java
@@ -0,0 +1,16 @@
+
+public class Looptest {
+
+	public static void main(String args[]){
+
+String x = "Dip, dip, potato chip";
+while(x=="Dip, dip, potato chip"){
+		System.out.println(x);
+		
+
+
+}
+	
+}
+	
+}
